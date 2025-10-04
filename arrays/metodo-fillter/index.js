18ab02731acbb533d60102600907c4d771fdc0f6
Kauã Felix <kaua90044@gmail.com>
2025-10-03 22:06:54 -0300

    Add examples for filter and map array methods
    Introduces two files demonstrating JavaScript array methods: filter to select numbers greater than 10, and map to double each number in an array.

---
+++ arrays/metodo-fillter/index.js
@@ -0,0 +1,5 @@
+const numeros =  [1, 34, 56, 78, 6, 7, 8, 9, 0, 23, 45, 67, 89];
+
+const numerosFilter = numeros.filter( numeros => numeros > 10);
+
+console.log(numerosFilter)
---
+++ arrays/metodo-map/index.js
@@ -0,0 +1,5 @@
+const numeros = [2, 6, 7, 8, 9, 10];
+
+const duplica =  numeros.map((x) => x*2);
+
+console.log(duplica);
